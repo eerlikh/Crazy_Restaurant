5e5b2f4c66b3003e8d16f286255638101958b00f
Eduard Erlikh <eerlikh7@gmail.com>
2015-08-24 16:35:50 -0400

    First Commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'pg'
+gem 'sinatra-activerecord'
+gem 'rake'
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra/activerecord'
+
+require 'bundler'
+Bundler.require
+
+get '/' do
+  "adsfd"
+end
