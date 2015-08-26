a462d3e7c078ebb769e9200338604b2e01527ef8
Eduard Erlikh <eerlikh7@gmail.com>
2015-08-26 17:23:01 -0400

    Finished the routing, created check boxes for my forms

--- controllers/orders_controller.rb
+++ controllers/orders_controller.rb
@@ -9,14 +9,27 @@ class OrdersController < ApplicationController
   # '/orders/new'
   get '/new' do
     @inventory_items = InventoryItem.all
-    @scary_rides = ScaryRide.all
+    @parties = Party.all
     erb :'orders/new'
   end
 
   # '/orders'
   post '/' do
-    Order.create(params[:order])
-    redirect '/'  
+    binding.pry
+    # {"order"=>{"party_id"=>"7"}, "inventory_items"=>["10", "11"]}
+
+    party_id = params['party_id']
+    inventory_items =  params['inventory_items']
+
+    inventory_items.each do |inventory_item|
+      Order.create({
+        party_id: party_id,
+        inventory_item_id: inventory_item
+        })
+    end
+
+    # Order.create(params[:order])
+    redirect '/'
   end
 
 
